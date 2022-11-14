ff65886083d2bb03d914c81d89cc882d4b0d5a6c
Sam Cooper <samuel.j.cooper@outlook.com>
2022-11-14 16:37:14 +0900

    added basic html templates

--- example/main.go
+++ example/main.go
@@ -5,11 +5,26 @@ import (
 	"github.com/scooper/go-getter/pkg/context"
 )
 
+type IndexData struct {
+	Title string
+	Heading string
+	Content string
+}
+
 func main() {
 	ctx := gg.CreateContext()
 
 	ctx.Route("/hello", "GET", func(request *context.Request) *context.Response {
 		return context.Text("Hello World!")
+	})
+
+	ctx.Route("/", "GET", func(request *context.Request) *context.Response {
+		// TODO: init index data and pass into function
+		return context.Template("index.html", &IndexData{
+			Title: "Example Index",
+			Heading: "Example Home",
+			Content: "Lorem ispum.....",
+		})
 	})
 
 	ctx.Start()
--- pkg/context/context.go
+++ pkg/context/context.go
@@ -1,7 +1,12 @@
 package context
 
 import (
+	"bytes"
+	"html/template"
+	"io"
 	"net/http"
+
+	"github.com/scooper/go-getter/pkg/utils"
 )
 
 type Request struct {
@@ -35,6 +40,7 @@ type Response struct {
 	StatusCode int
 	Headers map[string]string
 	Body string
+	Error error
 }
 
 func Text(content string) *Response {
@@ -45,6 +51,46 @@ func Text(content string) *Response {
 	}
 
 	response.Headers["Content-Type"] = "text/plain"
+
+	return response
+}
+
+func Template(filepath string, data any) *Response {
+
+	tfile, ferr := utils.GetTemplate(filepath)
+	
+	if ferr != nil {
+		return &Response {
+			StatusCode: 500,
+			Error: ferr,
+		}
+	}
+
+	defer tfile.Close()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, tfile)
+	fileAsStr := string(buf.Bytes())
+
+	t, terr := template.New("").Parse(fileAsStr)
+	if terr != nil {
+		return &Response {
+			StatusCode: 500,
+			Error: terr,
+		}
+	}
+
+	bodyBuf := new(bytes.Buffer)
+	
+	t.Execute(bodyBuf, data)
+
+	response := &Response {
+		StatusCode: 200, // TODO: easier way to change this?
+		Body: bodyBuf.String(),
+		Headers: make(map[string]string),
+	}
+
+	response.Headers["Content-Type"] = "text/html"
 
 	return response
 }
